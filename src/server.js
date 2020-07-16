a953fe1cba325091a62bfab86483e0200a9f880b
vinicios <viniciosvalensuela@gmail.com>
2020-07-16 16:59:45 -0300

    Cors configurado

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
-app.use(cors());
+app.use(cors({origin: true}));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
