6b7b842e92b059eb7afe0086cab1249e58dd1e90
vinicios <viniciosvalensuela@gmail.com>
2020-07-16 16:56:27 -0300

    Cors configurado

--- src/server.js
+++ src/server.js
@@ -12,6 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 mongoose.connect(databaseConfig.uri, {
     useUnifiedTopology: true,
